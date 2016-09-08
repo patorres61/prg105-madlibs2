238e6fabb69cd1ec10c67ab3738707260641336a
Phyllis Torres <ptorres@students.mchenry.edu>
2016-09-08 10:11:39 -0500

    Add files via upload

--- ptorres_madlibs2.py
+++ ptorres_madlibs2.py
@@ -1,50 +1,63 @@
-#Variables
+#Accept User Input Variables
 
 
-print("I need you to enter some words for this Madlib. There will be prompts for eleven variables. ")
+print("I need you to enter some words for this Madlib. There will be prompts for eight variables. ")
+print("If you can't come up with something, just hit enter and I will supply a variable for you.")
 
 
 print("Enter an adjective that descibes how one behaves, for example, timid, aggressive, loving, angry, etc.: ") #prompt for input of an adjective
 adjective = raw_input()
+if adjective == "":
+    adjective = "aggressive"
 
-print("\nEnter a person's job, e.g., policeman: ") #prompt for input of a person's job
+print("Enter a person's job, e.g., policeman: ") #prompt for input of a person's job
 job = raw_input()
+if job == "":
+    job = "librarian"
 
-print("\nEnter a type of insect: ") #prompt for input of an insect
+print("Enter a type of insect: ") #prompt for input of an insect
 insect = raw_input()
+if insect == "":
+    insect = "cockroach"
 
-print("\nEnter a type of arachnid, e.g., spider, scorpion, tick, mite, chigger: ") #prompt for a type of arachnid
-arachnid = raw_input()
-
-print("\nEnter a type of flying animal: ") #prompt for a flying animal
+print("Enter a type of flying animal: ") #prompt for a flying animal
 flyingAnimal = raw_input()
+if flyingAnimal == "":
+    flyingAnimal = "bat"
 
-print("\nEnter a type of pet: ") #prompt for a pet
+print("Enter a type of pet: ") #prompt for a pet
 pet = raw_input()
+if pet == "":
+    pet = "chinchilla"
 
-print("\nEnter a type of animal: ") #prompt for a type of animal
-animal = raw_input()
-
-print("\nEnter a regular action word that can end with -ed, e.g., kiss: ") #prompt for an action
+print("Enter a regular action word that can end with -ed, e.g., kiss, but don't add the -ed: ") #prompt for an action
 action = raw_input()
+if action == "":
+    action = "lick"
 
-print("\nEnter a very large animal: ") #prompt for a very large animal
+print("Enter a very large animal: ") #prompt for a very large animal
 largeAnimal = raw_input()
+if largeAnimal == "":
+    largeAnimal = "whale"
 
-print("\nEnter another action word that can end with -ed, e.g., play: ") #prompt for another action
+print("Enter another action word that can end with -ed, e.g., walk: ") #prompt for another action
 action2 = raw_input()
+if action2 == "":
+    action2 = "explode"
 
-print("\nEnter another type of pet: ") #prompt for another pet
-pet2 = raw_input()
+#Load remaining variables
+animal = "porcupine"
+arachnid = "scorpion"
+pet2 = "pot-belly pig"
 
-
-print("\nGood Job! I know I asked for a lot of variables!")
+print("\nGood Job! That was a lot of variables!")
 
 print("\nHere is your Madlib!")
 
 #poem
-print("\nMadlibs: There was a(n) " + adjective + " " + job + " who " + action + "ed a " + insect + "\n")
-print("There was an " + adjective + " " + job + " who " + action + "ed a " + insect + ",")
+print("\nMadlib: There was a(n) " + adjective + " " + job + " who " + action + "ed a " + insect + "\n")
+print("There was a(n)"
+      " " + adjective + " " + job + " who " + action + "ed a " + insect + ",")
 print("I don't know why she " + action + "ed a " + insect + " - perhaps she'll " + action2 + "!\n")
 
 print("There was a(n) " + adjective + " " + job + " who " + action + "ed a " + arachnid + ",")
